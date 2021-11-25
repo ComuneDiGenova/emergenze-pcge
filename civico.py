39df3a229d043ecb71b5edb0178283fa631aa5a0
Manuele Pesenti <manuele.pesenti@gter.it>
2021-11-25 18:06:25 +0100

    ricerca civico

--- civico.py
+++ civico.py
@@ -2,6 +2,44 @@
 
 from .common import db
 
+# from pydal.validators import *
+
+# class IS_NUM_IN_RANGE(IS_INT_IN_RANGE):
+#     """docstring for IS_NUM_IN_RANGE."""
+#
+#     def __init__(self, *args, digits=4, **kwargs):
+#         IS_INT_IN_RANGE.__init__(self, *args, **kwargs)
+#         self.digits = digits
+#
+#     def formatter(self, value):
+#         return f"{value:d}:0{self.digits:d}"
+#
+# class IS_LETTERA(IS_LENGTH):
+#     """docstring for IS_LETTERA."""
+#
+#     def __init__(
+#         self,
+#         maxsize=1,
+#         minsize=0,
+#         error_message="Inserire da %(min)g a %(max)g caratteri",
+#     ):
+#         IS_LENGTH.__init__(self, maxsize=maxsize, minsize=minsize, error_message=error_message)
+#         self.maxsize = maxsize
+#         self.minsize = minsize
+#         self.error_message = error_message
+#
+#     def formatter(self, value):
+#         return value.upper()
+
+# class IS_NULL_OR(IS_EMPTY_OR):
+#     """docstring for IS_NULL_OR."""
+#
+#     def formatter(self, value):
+#         value, empty = is_empty(value, empty_regex=self.empty_regex)
+#         if empty:
+#             return self.null
+#         return value
+
 
 def render(row):
     rec = {k: v for k,v in row.items() if not k.startswith('_')}
@@ -9,33 +47,40 @@ def render(row):
 
 
 def fetch(desvia=None, numero=None, lettera=None, colore=None, cap=None,
-    sounds_like=None, starts_with=None, near_by=None, page=0, paginate=None
+    sounds_like=None, starts_with=None, near_by=None, page=0, paginate=None,
+    epsg=4326
 ):
     """ """
 
     dbset = db(db.civico)
 
     if not numero is None:
-        dbset = dbset(db.civico.numero == f'{numero:d}')
+        dbset = dbset(db.civico.numero == f'{numero:04}')
     if not lettera is None:
         dbset = dbset(db.civico.lettera.lower() == f'{lettera:s}'.lower())
     if not colore is None:
         dbset = dbset(db.civico.colore.lower() == f'{colore:s}'.lower())
     if not cap is None:
-        dbset = dbset(db.civico.cap == f'{cap:d}')
+        dbset = dbset(db.civico.cap == f'{cap:05}')
     if not desvia is None:
+        cnts = []
         for substr in desvia.split():
-            dbset = dbset(db.civico.desvia.ilike(f'%{substr}%'))
+            cnts.append(f"CASE WHEN length(desvia) - length(replace(lower(desvia), lower('{substr}'), '')) > 0 THEN 1 ELSE 0 END")
+
     if not starts_with is None:
         dbset = dbset(db.civico.desvia.ilike(f'{starts_with}%'))
 
-    orderby = db.civico.numero | db.civico.lettera | db.civico.colore
+    orderby = db.civico.desvia | db.civico.numero | db.civico.lettera | db.civico.colore
     if not sounds_like is None:
         orderby = f"LEVENSHTEIN(lower('{sounds_like}'), lower(desvia)) ASC, "\
         f"{orderby}"
 
     if not near_by is None:
-        orderby = f"ST_Distance(geom, ST_Transform(ST_SetSRID(ST_GeomFromGeoJSON('{near_by}'), 4326), 3003)) ASC, {orderby}"
+
+        orderby = f"ST_Distance(geom, ST_Transform(ST_SetSRID(ST_GeomFromGeoJSON('{near_by}'), {epsg}), 3003)) ASC, {orderby}"
+
+    if not desvia is None:
+        orderby = f"{' + '.join(cnts)} DESC, {orderby}"
 
     fields = (
         db.civico.codvia.with_alias('codvia'),
@@ -53,8 +98,3 @@ def fetch(desvia=None, numero=None, lettera=None, colore=None, cap=None,
     ))
 
     return result if paginate is None or paginate>1 else next(result)
-
-
-
-if __name__ == '__main__':
-    rr = fetch()
--- controllers.py
+++ controllers.py
@@ -25,11 +25,28 @@ session, db, T, auth, and tempates are examples of Fixtures.
 Warning: Fixtures MUST be declared with @action.uses({fixtures}) else your app will result in undefined behavior
 """
 
-from py4web import action, request, abort, redirect, URL
+from py4web import action, request, abort, redirect, URL, Field
 from yatl.helpers import A
-from .common import session, T, cache, auth, logger, authenticated, unauthenticated, flash
+from .common import session, T, cache, auth, logger, authenticated, unauthenticated, flash, db
 
-from .import evento as _evento
+from py4web.utils.form import Form
+from pydal.validators import *
+
+from . import evento as _evento
+from . import civico as _civico
+
+from mptools.frameworks.py4web import shampooform as sf
+
+import geojson
+
+# from py4web.core import Fixture
+#
+# class query2forms(Fixture):
+#     def on_request(self):
+#         if request.query:
+#             for k in dict(request.query):
+#                 request.forms[k] = request.query.pop(k)
+
 
 # @unauthenticated("index", "index.html")
 # def index():
@@ -40,3 +57,62 @@ from .import evento as _evento
 @action("evento")
 def evento():
     return {'result': _evento.fetch()}
+
+@action('civico', method=['GET', 'POST'])
+# @action.uses(query2forms())
+def civico():
+    db.civico.desvia.comment = 'Cerca per toponimo'
+    res = db(db.civico).select(
+        db.civico.numero.min().with_alias('nummin'),
+        db.civico.numero.max().with_alias('nummax'),
+        db.civico.cap.min().with_alias('capmin'),
+        db.civico.cap.max().with_alias('capmax'),
+    ).first()
+    # db.civico.colore.requires = IS_IN_DB(db(db.civico), db.civico.colore, distinct=True)
+    db.civico.colore.requires = IS_EMPTY_OR(IS_IN_SET([
+        ('','Nero'),
+        ('R','Rosso'),
+        ('r', 'Rosso')
+    ]))
+    db.civico.lettera.requires = IS_LENGTH(1)
+
+    form = Form([
+        db.civico.desvia,
+        Field(db.civico.numero.name, 'integer',
+            label = db.civico.numero.label,
+            comment = f"Numero compreso tra {int(res.nummin):d} e {int(res.nummax):d}",
+            length = db.civico.numero.length,
+            requires = IS_EMPTY_OR(IS_INT_IN_RANGE(int(res.nummin), int(res.nummax)))
+        ),
+        db.civico.lettera,
+        db.civico.colore,
+        Field(db.civico.cap.name, 'integer',
+            label = db.civico.cap.label,
+            comment = f"Numero compreso tra {int(res.capmin):d} e {int(res.capmax):d}",
+            length = db.civico.cap.length,
+            requires = IS_EMPTY_OR(IS_INT_IN_RANGE(int(res.capmin), int(res.capmax)))
+        ),
+        Field('lon', 'double', label='Longitude', requires=IS_EMPTY_OR(IS_FLOAT_IN_RANGE(-180., 180.))),
+        Field('lat', 'double', label='Latitude', requires=IS_EMPTY_OR(IS_FLOAT_IN_RANGE(-90., 90.))),
+        # Field('epsg', )
+        Field('page', 'integer', default=0, requires=IS_EMPTY_OR(IS_INT_IN_RANGE())),
+        Field('paginate', 'integer', default=10, requires=IS_EMPTY_OR(IS_INT_IN_RANGE())),
+    ], deletable = False, dbio=False,
+        form_name = 'civico',
+        csrf_protection = False
+    )
+    result = None
+    if form.accepted:
+
+        for field in filter(lambda ff: not ff.name in form.errors, form.table):
+            if form.vars[field.name] is None:
+                if not field.default is None:
+                    form.vars[field.name] = field.default
+
+        lon = form.vars.pop('lon')
+        lat = form.vars.pop('lat')
+        if not None in (lon, lat,):
+            form.vars["near_by"] = geojson.Point([lon, lat])
+        result = _civico.fetch(**form.vars)
+
+    return {'result': result, 'form': sf.form2dict(form)}
