f061ec6dca4bd2dc59508df1094754eaca53862d
Manuele Pesenti <manuele.pesenti@gter.com>
2022-03-10 19:08:26 +0100

    dev invio allegato comunicazione su presidio

--- presidio_mobile/comunicazione.py
+++ presidio_mobile/comunicazione.py
@@ -4,21 +4,26 @@ import os
 from ..common import db, logger
 from ..verbatel import Presidio
 
+from .. import settings
+from py4web import Field
+import base64
+
 def render(row):
     """ """
 
-    # TODO:
-    # import pdb; pdb.set_trace()
-    # allegato = {
-    #     'fileName': os.path.basename(row.comunicazione.allegato),
-    #     'file': '' 
-    # }
+    with open(os.path.join(settings.EMERGENZE_UPLOAD, *(row.comunicazione_presidio.allegato.split(os.path.sep)[1:])), 'rb') as ff:
+        encoded_string = base64.b64encode(ff.read())
+
+    allegato = {
+        'fileName': os.path.basename(row.comunicazione_presidio.allegato),
+        'file': encoded_string 
+    }
 
     return {
         # 'idSquadra': row.idSquadra,
         'operatore': 'anonimo',
         'testo': row.testo,
-        'files': []
+        'files': [allegato]
     }
     
 
