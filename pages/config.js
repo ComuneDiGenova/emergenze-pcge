152995e3dd039e4191099949f569b8b7573e1f98
Gter <lbenvenuto91@github.com>
2023-06-05 10:16:20 +0200

    fix permessi e configurazione chiamate

--- pages/config.js
+++ pages/config.js
@@ -1,5 +1,15 @@
-const config = {
-  BASE_URL: "https://emergenze-apit.comune.genova.it/emergenze/",
-  LOCAL_URL: "http://localhost:8000/emergenze/",
-  DB_URL: "https://emergenze-apit.comune.genova.it/emergenze/",
-};
+if ( $_SERVER['HTTP_HOST'] == 'vm-lxprotcivemet.comune.genova.it' ) {
+    const config = {
+        BASE_URL: "https://emergenze-api.comune.genova.it/emergenze/",
+        LOCAL_URL: "http://localhost:8000/emergenze/",
+        DB_URL: "https://emergenze-api.comune.genova.it/emergenze/",
+    };
+} else {
+    const config = {
+        BASE_URL: "https://emergenze-apit.comune.genova.it/emergenze/",
+        LOCAL_URL: "http://localhost:8000/emergenze/",
+        DB_URL: "https://emergenze-apit.comune.genova.it/emergenze/",
+    };
+}
+
+
