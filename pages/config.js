cf47ebd34642fb70d94e4708b2a33cb7657702b9
Gter <assistenzagis@gter.it>
2023-07-21 16:15:46 +0200

    a better config

--- pages/config.js
+++ pages/config.js
@@ -1,15 +1,14 @@
-if ( $_SERVER['HTTP_HOST'] == 'vm-lxprotcivemet.comune.genova.it' ) {
-    const config = {
+let config;
+if ( window.location.host == 'emergenze.comune.genova.it' ) {
+    config = {
         BASE_URL: "https://emergenze-api.comune.genova.it/emergenze/",
         LOCAL_URL: "http://localhost:8000/emergenze/",
         DB_URL: "https://emergenze-api.comune.genova.it/emergenze/",
     };
 } else {
-    const config = {
+    config = {
         BASE_URL: "https://emergenze-apit.comune.genova.it/emergenze/",
         LOCAL_URL: "http://localhost:8000/emergenze/",
         DB_URL: "https://emergenze-apit.comune.genova.it/emergenze/",
     };
-}
-
-
+};
