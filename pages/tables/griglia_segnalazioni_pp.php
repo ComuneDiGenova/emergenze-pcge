bb6299fcdd04ea1f3624570333ebe7080deedb0f
Gter <info@gter.it>
2023-12-22 11:38:19 +0100

    risolta evolutiva 8 Segnalare l'UO che ha in carico l'evento

--- pages/tables/griglia_segnalazioni_pp.php
+++ pages/tables/griglia_segnalazioni_pp.php
@@ -15,23 +15,36 @@ if(isset($_GET["f"])){
 }
 
 
-$filter= " WHERE (s.in_lavorazione = 't' or s.in_lavorazione is null) and (s.fine_sospensione is null OR s.fine_sospensione < now()) ";
-
-
 if(!$conn) {
     die('Connessione fallita !<br />');
 } else {
-	//$idcivico=$_GET["id"];
-	$query="SELECT s.id, s.criticita, s.id_evento,
-       s.num, s.in_lavorazione, s.localizzazione, s.nome_munic, st_x(s.geom) as lon, st_y(s.geom) as lat, s.incarichi 
-       FROM segnalazioni.v_segnalazioni_lista_pp s
-       JOIN segnalazioni.join_segnalazioni_in_lavorazione j ON s.id_lavorazione=j.id_segnalazione_in_lavorazione ".$filter." and j.sospeso='f';";
-     
-   //echo $query;
-	//echo "<br>";
+	// $idcivico=$_GET["id"];
+	// $query="SELECT s.id, s.criticita, s.id_evento,
+    //    s.num, s.in_lavorazione, s.localizzazione, s.nome_munic, st_x(s.geom) as lon, st_y(s.geom) as lat, s.incarichi, s.id_profilo
+    //    FROM segnalazioni.v_segnalazioni_lista_pp s
+    //    JOIN segnalazioni.join_segnalazioni_in_lavorazione j ON s.id_lavorazione=j.id_segnalazione_in_lavorazione 
+	//    WHERE (s.in_lavorazione = 't' or s.in_lavorazione is null) 
+	//    	and (s.fine_sospensione is null OR s.fine_sospensione < now()) 
+	// 	and j.sospeso='f';";
+
+	$query = "SELECT s.id, s.criticita, s.id_evento, s.num, s.in_lavorazione, s.localizzazione, s.nome_munic, 
+					st_x(s.geom) as lon, st_y(s.geom) as lat,
+					s.incarichi,
+					string_agg(case when i.id_stato_incarico::varchar = '2' then i.descrizione_uo::varchar else null end,	' - ') responsabile_incarico
+				FROM segnalazioni.v_segnalazioni_lista_pp s
+				JOIN segnalazioni.join_segnalazioni_in_lavorazione j 
+					ON s.id_lavorazione=j.id_segnalazione_in_lavorazione
+				LEFT JOIN segnalazioni.v_incarichi i
+					ON s.id_lavorazione=i.id_lavorazione
+				WHERE (s.in_lavorazione = 't' or s.in_lavorazione is null) 
+					and (s.fine_sospensione is null OR s.fine_sospensione < now()) 
+					and j.sospeso='f'
+				GROUP BY s.id, s.criticita, s.id_evento,
+						s.num, s.in_lavorazione, s.localizzazione, 
+						s.nome_munic, lon, lat, s.incarichi;";
+
 	$result = pg_query($conn, $query);
-	#echo $query;
-	#exit;
+
 	$rows = array();
 	while($r = pg_fetch_assoc($result)) {
     		$rows[] = $r;
