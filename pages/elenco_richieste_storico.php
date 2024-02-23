f7fc8593169c13b3460404db68d5b6262ff6d792
Gter <info@gter.it>
2024-02-23 11:33:18 +0100

    modificata pagina storico per renderla riconoscivile dalle richieste attive

--- pages/elenco_richieste_storico.php
+++ pages/elenco_richieste_storico.php
@@ -42,7 +42,7 @@ require('navbar_up.php');
         <div id="page-wrapper">
             <div class="row">
                 <div class="col-lg-12">
-                    <h1 class="page-header">Richieste ricevute</h1>
+                    <h1 class="page-header">Richieste ricevute (storico)</h1>
                 </div>
                 <!-- /.col-lg-12 -->
             </div>
