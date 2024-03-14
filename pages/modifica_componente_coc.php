f933042e5203715b4e2c100ea6ceac962024ac56
Gter <info@gter.it>
2024-03-14 16:23:19 +0100

    reformatting query

--- pages/modifica_componente_coc.php
+++ pages/modifica_componente_coc.php
@@ -15,14 +15,12 @@ if(!$conn) {
         $telegram=pg_escape_string($_POST["telegramId"]);
         $funzione=pg_escape_string($_POST["addFunzione"]);
 
-        $query = "UPDATE users.utenti_coc SET
-            matricola_cf = $1, nome = $2, cognome = $3, mail = $4, telegram_id = $5, funzione = $6
-            where id = $7;";
-        //$result = pg_query($conn, $query);
-        //echo $query_lizmap;
+        $query = "UPDATE users.utenti_coc 
+                SET matricola_cf = $1, nome = $2, cognome = $3, mail = $4, telegram_id = $5, funzione = $6
+                WHERE id = $7;";
+
         $result = pg_prepare($conn, "myquery", $query);
         $result = pg_execute($conn, "myquery", array($cf, $nome, $cognome, $mail, $telegram, $funzione, $id));
-
 
         header ("Location: lista_utenti_coc.php");
     }
