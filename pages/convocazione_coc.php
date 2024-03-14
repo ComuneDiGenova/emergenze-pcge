a9f524d841d0b3d5e2fb417079c657c0791ed769
Gter <info@gter.it>
2024-03-14 09:48:44 +0100

    refactoring query

--- pages/convocazione_coc.php
+++ pages/convocazione_coc.php
@@ -17,22 +17,22 @@ require('./token_telegram.php');
 
 require('./send_message_telegram.php');
 
-$query="SELECT distinct on (u.telegram_id) u.matricola_cf,
-u.nome,
-u.cognome,
-jtfc.funzione,
-u.telegram_id,
-tp.id,
-tp.data_invio,
-tp.lettura,
-tp.data_conferma,
-tp.data_invio_conv,
-tp.data_conferma_conv,
-tp.lettura_conv 
-FROM users.utenti_coc u
-right JOIN users.t_convocazione tp ON u.telegram_id::text = tp.id_telegram::text
-join users.tipo_funzione_coc jtfc on jtfc.id = u.funzione
-order by u.telegram_id, tp.data_invio desc;";
+$query="SELECT DISTINCT ON (u.telegram_id) u.matricola_cf,
+							u.nome,
+							u.cognome,
+							jtfc.funzione,
+							u.telegram_id,
+							tp.id,
+							tp.data_invio,
+							tp.lettura,
+							tp.data_conferma,
+							tp.data_invio_conv,
+							tp.data_conferma_conv,
+							tp.lettura_conv 
+		FROM users.utenti_coc u
+		RIGHT JOIN users.t_convocazione tp ON u.telegram_id::text = tp.id_telegram::text
+		JOIN users.tipo_funzione_coc jtfc ON jtfc.id = u.funzione
+		ORDER BY u.telegram_id, tp.data_invio DESC;";
 
 $result = pg_prepare($conn, "myquery", $query);
 $result = pg_execute($conn, "myquery", array());
