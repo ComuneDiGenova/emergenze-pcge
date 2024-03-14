5d5ef9679e97adbd17da72521867f583251d7a69
Gter <info@gter.it>
2024-03-14 19:24:33 +0100

    reformat

--- pages/tables/griglia_convocazione_coc.php
+++ pages/tables/griglia_convocazione_coc.php
@@ -40,7 +40,6 @@ if(!$conn) {
 	$rows = array();
 	while($r = pg_fetch_assoc($result)) {
     		$rows[] = $r;
-    		//$rows[] = $rows[]. "<a href='puntimodifica.php?id=" . $r["NAME"] . "'>edit <img src='../../famfamfam_silk_icons_v013/icons/database_edit.png' width='16' height='16' alt='' /> </a>";
 	}
 	pg_close($conn);
 	#echo $rows ;
