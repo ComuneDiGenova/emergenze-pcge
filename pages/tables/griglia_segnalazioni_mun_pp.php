84c96b8ad04cc25681d82afa650274e727194f18
Gter <info@gter.it>
2024-03-19 17:10:40 +0100

    eliminati commenti inutili

--- pages/tables/griglia_segnalazioni_mun_pp.php
+++ pages/tables/griglia_segnalazioni_mun_pp.php
@@ -8,7 +8,6 @@ include explode('emergenze-pcge',getcwd())[0].'emergenze-pcge/conn.php';
 // Filtro per tipologia di criticità
 if(isset($_GET["f"])){
 	$getfiltri=$_GET["f"];
-	//echo $getfiltri;
 
 	require('./filtri_segnalazioni.php'); //contain the function filtro used in the following line
 	$filter=filtro($getfiltri);
@@ -18,17 +17,6 @@ if(isset($_GET["f"])){
 if(!$conn) {
     die('Connessione fallita !<br />');
 } else {
-	//$idcivico=$_GET["id"];
-	// $query="SELECT s.id, s.criticita, s.id_evento, s.num, s.in_lavorazione, s.localizzazione, s.nome_munic, 
-	// 				st_x(s.geom) as lon, st_y(s.geom) as lat, 
-	// 				s.incarichi, s.id_profilo
-	// 		FROM segnalazioni.v_segnalazioni_lista_pp s
-	// 		JOIN segnalazioni.join_segnalazioni_in_lavorazione j 
-	// 			ON s.id_lavorazione=j.id_segnalazione_in_lavorazione 
-	// 		WHERE (s.in_lavorazione = 't' OR s.in_lavorazione is null) 
-	// 			AND (s.fine_sospensione is null OR s.fine_sospensione < now()) 
-	// 			AND j.sospeso='t';";
-
 	$query="SELECT s.id, s.criticita, s.id_evento, s.num, s.in_lavorazione, s.localizzazione, s.nome_munic, 
 					st_x(s.geom) as lon, st_y(s.geom) as lat, 
 					s.incarichi, s.id_profilo,
@@ -49,20 +37,16 @@ if(!$conn) {
 					s.num, s.in_lavorazione, s.localizzazione, 
 					s.nome_munic, lon, lat, s.incarichi, s.id_profilo;";
     
-   //echo $query;
-	//echo "<br>";
 	$result = pg_query($conn, $query);
-	#echo $query;
-	#exit;
+
 	$rows = array();
 	while($r = pg_fetch_assoc($result)) {
     		$rows[] = $r;
-    		//$rows[] = $rows[]. "<a href='puntimodifica.php?id=" . $r["NAME"] . "'>edit <img src='../../famfamfam_silk_icons_v013/icons/database_edit.png' width='16' height='16' alt='' /> </a>";
 	}
+
 	pg_close($conn);
-	#echo $rows ;
+
 	if (empty($rows)==FALSE){
-		//print $rows;
 		print json_encode(array_values(pg_fetch_all($result)));
 	} else {
 		echo "[{\"NOTE\":'No data'}]";
