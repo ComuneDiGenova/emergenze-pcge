f1d363c79f7d4d19f91d8958cbc0824431637279
Manuele Pesenti <manuele.pesenti@gmail.com>
2024-03-27 22:07:22 +0100

    fix: running bot

--- telegram/emergenze/src/emergenze/emergenze/settings.py
+++ telegram/emergenze/src/emergenze/emergenze/settings.py
@@ -1,6 +1,14 @@
-from .. import settings as __settings
+import os
+from .. import settings as _settings
 
-LOGGERS = __settings.LOGGERS
+LOGGERS = _settings.LOGGERS
 
-APP_NAME = __settings.BOT_PROPS['emergenze']['app_name']
-BOT_TOKEN = __settings.BOT_PROPS['emergenze']['bot_token']
+APP_NAME = _settings.BOT_PROPS['emergenze']['app_name']
+BOT_TOKEN = _settings.BOT_PROPS['emergenze']['bot_token']
+
+class conn(object):
+    ip = os.environ.get('conn_ip')
+    db = os.environ.get('conn_db')
+    user = os.environ.get('conn_user')
+    pwd = os.environ.get('conn_pwd')
+    port = os.environ.get('conn_port')
