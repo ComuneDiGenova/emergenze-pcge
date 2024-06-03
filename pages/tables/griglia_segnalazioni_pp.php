f4a4bb4fe115f7c17cf39843e010f20a34d27ce5
Gter <info@gter.it>
2024-06-03 17:37:13 +0200

    ticket risolto

--- pages/tables/griglia_segnalazioni_pp.php
+++ pages/tables/griglia_segnalazioni_pp.php
@@ -27,31 +27,29 @@ if(!$conn) {
 	//    	and (s.fine_sospensione is null OR s.fine_sospensione < now()) 
 	// 	and j.sospeso='f';";
 
-	$query = "SELECT s.id, s.criticita, s.id_evento, s.num, s.in_lavorazione, s.localizzazione, s.nome_munic, 
+	$query = "SELECT main.id, main.criticita, main.id_evento, main.num, main.in_lavorazione, main.localizzazione, main.nome_munic, 
+					main.lon, main.lat,
+					main.incarichi, string_agg(main.responsabile_incarico, ' - ') 
+				from (SELECT s.id, s.criticita, s.id_evento, s.num, s.in_lavorazione, s.localizzazione, s.nome_munic, 
 					st_x(s.geom) as lon, st_y(s.geom) as lat,
 					s.incarichi,
-					STRING_AGG(DISTINCT
-						CASE 
-							WHEN i.id_stato_incarico::varchar = '2' THEN i.descrizione_uo::varchar
-							ELSE NULL 
-						END, ' - ') || ' - ' || 
-					STRING_AGG(DISTINCT
-						CASE 
-							WHEN ii.id_stato_incarico::varchar = '2' THEN ii.descrizione_uo::varchar
-							ELSE NULL 
-						END, ' - ') AS responsabile_incarico
+					unnest(array_agg(distinct i.descrizione_uo::varchar) || array_agg(distinct ii.descrizione_uo::varchar)
+								) as responsabile_incarico		
 				FROM segnalazioni.v_segnalazioni_lista_pp s
 				JOIN segnalazioni.join_segnalazioni_in_lavorazione j 
 					ON s.id_lavorazione=j.id_segnalazione_in_lavorazione
 				LEFT JOIN segnalazioni.v_incarichi i
-					ON s.id_lavorazione = i.id_lavorazione AND i.id_stato_incarico::varchar = '2'
+					ON s.id_lavorazione=i.id_lavorazione
 				LEFT JOIN segnalazioni.v_incarichi_interni ii
-					ON s.id_lavorazione = ii.id_lavorazione AND ii.id_stato_incarico::varchar = '2'
-					WHERE (s.in_lavorazione = 't' OR s.in_lavorazione IS NULL) 
-						AND (s.fine_sospensione IS NULL OR s.fine_sospensione < NOW()) 
-						AND j.sospeso = 'f'
-				GROUP BY s.id, s.criticita, s.id_evento, s.num, s.in_lavorazione, s.localizzazione, s.nome_munic, 
-					lon, lat, s.incarichi;";
+					ON s.id_lavorazione=ii.id_lavorazione
+				WHERE (s.in_lavorazione = 't' or s.in_lavorazione is null) 
+					and (s.fine_sospensione is null OR s.fine_sospensione < now()) 
+					and j.sospeso='f'
+				GROUP BY s.id, s.criticita, s.id_evento,
+						s.num, s.in_lavorazione, s.localizzazione, 
+						s.nome_munic, lon, lat, s.incarichi) AS main
+				group by main.id, main.criticita, main.id_evento, main.num, main.in_lavorazione, main.localizzazione, 
+						main.nome_munic, lon, lat, main.incarichi;";
 
 	$result = pg_query($conn, $query);
 
@@ -63,7 +61,7 @@ if(!$conn) {
 	pg_close($conn);
 	#echo $rows ;
 	if (empty($rows)==FALSE){
-		//print $rows;
+		print $rows;
 		print json_encode(array_values(pg_fetch_all($result)));
 	} else {
 		echo "[{\"NOTE\":'No data'}]";
