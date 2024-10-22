a11f5c7ff37e96bdf0749d874b50c9252cb714cf
Gter <assistenzagis@gter.it>
2024-10-22 11:05:32 +0200

    patch: faster cdn for tableExport and removed non conda deps from readxml requirements

--- pages/req_bottom.php
+++ pages/req_bottom.php
@@ -22,7 +22,7 @@ $subtitle2 = str_replace("'", "\'", str_replace(' ', '_', $subtitle));
 <script src="../vendor/bootstrap-select/dist/js/bootstrap-select.js"></script>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/list.js/1.1.1/list.min.js"></script>
-<script src="//rawgit.com/hhurz/tableExport.jquery.plugin/master/tableExport.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/tableexport.jquery.plugin@1.30.0/tableExport.min.js" integrity="sha256-JyCQ2nRcfgpZ59ajhyVPIcC7FLX3UUaWDX8dXJwLHWg=" crossorigin="anonymous"></script>
 
 <!-- Leaflet JavaScript -->
 <script src="../vendor/leaflet/leaflet.js"></script>
