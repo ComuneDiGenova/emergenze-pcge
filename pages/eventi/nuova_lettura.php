14a6cc5c72fcbb38b240757c1174445ec9a1316e
Gter <info@gter.it>
2024-11-08 18:10:14 +0100

    migliorato formatting dati di nuova_lettura

--- pages/eventi/nuova_lettura.php
+++ pages/eventi/nuova_lettura.php
@@ -3,33 +3,27 @@
 	require('../validate_input.php');
 
 	include explode('emergenze-pcge',getcwd())[0].'emergenze-pcge/conn.php';
-
+	
 	$id = str_replace("'", "", $_GET["id"]);
-	$tipo = $_POST["tipo"];
+	$id_lettura = $_POST["tipo"];
 	$hh_start = $_POST["hh_start"];
 	$mm_start = $_POST["mm_start"];
-	$utente = $_SESSION["Utente"]
+	$utente = $_SESSION["Utente"];
 
 	// Crea la data/ora combinando la data corrente con ora e minuti dal form
-	$data_inizio = date('Y-m-d') . ' ' . $hh_start . ':' . $mm_start;
+	$data_ora = date('Y-m-d') . ' ' . $hh_start . ':' . $mm_start;
 
 	// Inserisci la lettura a DB
-	$query = "INSERT INTO geodb.lettura_mire (num_id_mira, id_lettura, data_ora) VALUES ($id, $tipo, $data_inizio)";
+	$query = "INSERT INTO geodb.lettura_mire (num_id_mira, id_lettura, data_ora) VALUES ($id, $id_lettura, '$data_ora')";
     $result = pg_query($conn, $query);
 
-	if (!$result) {
-        throw new Exception("Errore nell'inserimento della lettura.");
-    }
-	
+	echo $query;
 	// Inserisci il log dell'operazione
 	$log_message = "Inserita lettura mira . " . $id;
 	$query_log = "INSERT INTO varie.t_log (schema, operatore, operazione) VALUES ('geodb', $utente, $log_message)";
     $result_log = pg_query($conn, $query_log);
 
-	if (!$result_log) {
-        throw new Exception("Errore nell'inserimento del log.");
-    }
-
 	// Ricarico la pagina
 	header("location: ../mire.php");
+	exit;
 ?>
