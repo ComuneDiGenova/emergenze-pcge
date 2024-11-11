2ed439097e9a3749e0033b1d656782cf8fe563e9
Gter <info@gter.it>
2024-11-11 22:35:32 +0100

    implementata logica per convocazione coc con bollettino, migliorata qualità codice

--- pages/convocazione_coc.php
+++ pages/convocazione_coc.php
@@ -10,18 +10,25 @@ $boll_pc = $_POST['boll_pc'];
 require('./token_telegram.php');
 require('./send_message_telegram.php');
 
-// Query per ottenere gli id telegram
+
+// Funzione per eseguire le query
+function execute_query($conn, $query) {
+    $result = pg_query($conn, $query);
+    if (!$result) {
+        error_log("Errore nella query: " . pg_last_error($conn));
+        exit("Errore nella query");
+    }
+    return $result;
+}
+
+// Ottengo gli ID Telegram
 $query_tg_ids = "SELECT DISTINCT ON (u.telegram_id) u.telegram_id
 				 FROM users.utenti_coc u;";
 
-// Preparazione ed esecuzione della query per ottenere gli id telegram
-$result_tg_ids = pg_prepare($conn, "get_tg_ids", $query_tg_ids);
-$result_tg_ids = pg_execute($conn, "get_tg_ids", array());
-
-// Estrai tutti gli id telegram come array
+$result_tg_ids = execute_query($conn, $query_tg_ids);
 $tg_ids = pg_fetch_all($result_tg_ids);
 
-// ottengo il nuovo valore di id_convocazione
+// Ottengo il nuovo valore di id_convocazione
 $query_id_convocazione = "SELECT COALESCE(
 								(SELECT id_convocazione 
 								FROM users.t_lettura_conv_coc
@@ -30,59 +37,65 @@ $query_id_convocazione = "SELECT COALESCE(
 								0
 							) AS id_convocazione;";
 
-// Estraggo l'id_convocazione
 $result = pg_query($conn, $query_id_convocazione);
 $row = pg_fetch_assoc($result);
 $id_convocazione = $row['id_convocazione'] + 1;
 
-$values = [];
-foreach ($tg_ids as $row) {
-	$chat_id_coc = $row['telegram_id'];
-
-	// Creo convocazione senza inserire bollettino
-	$values[] = "(
-		$id_convocazione,
-		date_trunc('hour', now()) + date_part('minute', now())::int / 10 * interval '10 min',
-		'$chat_id_coc'
-	)";
-}
+// Creo i record in `t_lettura_conv_coc`
+$values = array_map(function($row) use ($id_convocazione, $boll_pc) {
+    $chat_id_coc = $row['telegram_id'];
+    $id_bollettino_value = $boll_pc != 0 ? $boll_pc : 'NULL';
+    return "($id_convocazione, date_trunc('hour', now()) + date_part('minute', now())::int / 10 * interval '10 min', '$chat_id_coc', $id_bollettino_value)";
+}, $tg_ids);
 
 
 // Unisco tutti i valori in una singola stringa separata da virgole
 $values_string = implode(", ", $values);
 
-$query = "INSERT INTO users.t_lettura_conv_coc (id_convocazione, data_invio_conv, id_telegram) 
+$query = "INSERT INTO users.t_lettura_conv_coc (id_convocazione, data_invio_conv, id_telegram, id_bollettino) 
 			VALUES $values_string;";
-// Eseguo la query
-$result = pg_query($conn, $query);
 
-echo $boll_pc;
-echo "\n";
-echo $id_convocazione;
-echo "\n";
+execute_query($conn, $query);
 
 
+// Tento di aggiornare `t_lettura_bollettino`
+if ($boll_pc != 0) {
+    $query_update_bollettino = "UPDATE users.t_lettura_bollettino
+        						SET id_convocazione = $id_convocazione
+        						WHERE id_bollettino = $boll_pc 
+          							AND id_convocazione IS NULL;";
+
+	$result_update = pg_query($conn, $query_update_bollettino);
+
+	// se l'UPDATE non ha aggiornato record duplico le righe
+	if (pg_affected_rows($result_update) === 0) { 
+		$query_duplicate_rows = "INSERT INTO users.t_lettura_bollettino (id_bollettino, id_telegram, data_invio, data_conferma, lettura, id_convocazione)
+        						SELECT id_bollettino, id_telegram, data_invio, data_conferma, lettura, NULL
+        						FROM users.t_lettura_bollettino
+        						WHERE id_bollettino = $boll_pc;";
+		execute_query($conn, $query_duplicate_rows);
+
+		// Eseguo di nuovo l'UPDATE per assegnare l'id_convocazione ai nuovi record
+		execute_query($conn, $query_update_bollettino);
+	}
+}
 
 // Invio messaggio telegram per ogni telegram id del coc
-while($r = pg_fetch_assoc($result_tg_ids)) {
-	  
-	  	$keyboard = [
-			'inline_keyboard' => [
-				[
-					['text' => 'OK', 'callback_data' => 'convocazione']
-				]
-			]
-		];
-		$encodedKeyboard = json_encode($keyboard);
-		$parameters = 
-			array(
-				'chat_id' => $r['telegram_id'], 
-				'text' => $testo, 
-				'reply_markup' => $encodedKeyboard
-			);
-		
-		sendButton('sendMessage', $parameters, $tokencoc);
-  }
+foreach ($tg_ids as $row) {
+    $chat_id_coc = $row['telegram_id'];
+    $keyboard = [
+        'inline_keyboard' => [
+            [['text' => 'OK', 'callback_data' => 'convocazione']]
+        ]
+    ];
+    $encodedKeyboard = json_encode($keyboard);
+    $parameters = [
+        'chat_id' => $chat_id_coc,
+        'text' => $testo,
+        'reply_markup' => $encodedKeyboard
+    ];
+    sendButton('sendMessage', $parameters, $tokencoc);
+}
 
 header("Location: elenco_coc.php");
 exit();
