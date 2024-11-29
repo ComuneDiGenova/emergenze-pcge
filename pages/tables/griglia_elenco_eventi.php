3db56d694b8fec5c8422a47084aed4c1bb98c7c5
Gter <info@gter.it>
2024-11-29 14:44:02 +0100

    migliorata leggibilità

--- pages/tables/griglia_elenco_eventi.php
+++ pages/tables/griglia_elenco_eventi.php
@@ -5,29 +5,29 @@
 
 	if(!$conn) {
 		die('Connessione fallita !<br />');
+	
+	}
+
+	$query="SELECT e.id, e.descrizione, 
+					to_char(e.data_ora_inizio_evento, 'YYYY/MM/DD HH24:MI'::text) AS data_ora_inizio_evento, 
+					to_char(e.data_ora_fine_evento, 'YYYY/MM/DD HH24:MI'::text) AS data_ora_fine_evento, 
+					e.valido, n.nota 
+			FROM eventi.v_eventi e 
+			LEFT JOIN eventi.t_note_eventi n ON n.id_evento = e.id 
+			ORDER by data_ora_inizio_evento DESC;";
+	
+	$result = pg_query($conn, $query);
+
+	$rows = array();
+	while($r = pg_fetch_assoc($result)) {
+			$rows[] = $r;
+	}
+	pg_close($conn);
+
+	if (empty($rows)==FALSE){
+		print json_encode(array_values(pg_fetch_all($result)));
 	} else {
-
-		$query="SELECT e.id, e.descrizione, 
-						to_char(e.data_ora_inizio_evento, 'YYYY/MM/DD HH24:MI'::text) AS data_ora_inizio_evento, 
-						to_char(e.data_ora_fine_evento, 'YYYY/MM/DD HH24:MI'::text) AS data_ora_fine_evento, 
-						e.valido, n.nota 
-				FROM eventi.v_eventi e 
-				LEFT JOIN eventi.t_note_eventi n ON n.id_evento = e.id 
-				ORDER by data_ora_inizio_evento DESC;";
-		
-		$result = pg_query($conn, $query);
-
-		$rows = array();
-		while($r = pg_fetch_assoc($result)) {
-				$rows[] = $r;
-		}
-		pg_close($conn);
-
-		if (empty($rows)==FALSE){
-			print json_encode(array_values(pg_fetch_all($result)));
-		} else {
-			echo "[{\"NOTE\":'No data'}]";
-		}
+		echo '[{"NOTE":"No data"}]';
 	}
 
 ?>
