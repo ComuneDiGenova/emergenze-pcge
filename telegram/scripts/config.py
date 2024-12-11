df07dc0f4db9e11141ac8606c047dc09ae7cf50b
Gter <info@gter.it>
2024-12-11 16:27:47 +0100

    rimosso vecchi bot

---
+++ telegram/scripts/config.py
@@ -0,0 +1,6 @@
+TOKEN = "923781873:AAF1arJcUjhL4rcMwhAoQ2C0lenBCOUenDY" #da sostituire
+TOKEN_COC = "2078154730:AAGE6Y1_bf5SYvVLXXjwQk-3j5nSD8jjxUg" #token bot coc test
+
+website = "https://gestemert.comune.genova.it/emergenze"
+link = "https://gestemert.comune.genova.it/emergenze"
+
