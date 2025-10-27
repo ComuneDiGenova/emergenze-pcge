bca1e661b4689b3aa1967da4dc60f5ff3bbebee1
Gter <info@gter.it>
2025-10-27 18:00:23 +0100

    aggiunto nome evento sul menu nuovo sopralluogo e nuovo sopralluogo mobile

--- pages/nuovo_sopralluogo.php
+++ pages/nuovo_sopralluogo.php
@@ -76,13 +76,18 @@ require('navbar_up.php');
                 <select readonly="" class="form-control"  name="evento" required>
                  
                     <?php 
-                     for ($i=0;$i<$len;$i++){
-                      
-                        echo '<option name="evento" value="'.$tipo_eventi_attivi[0][0].'">'. $tipo_eventi_attivi[0][1].' (id='.$tipo_eventi_attivi[0][0].')</option>';
-                      }
+                        for ($i=0;$i<$len;$i++){
+                            $nota0 = trim((string)($nota_eventi_attivi[0][1] ?? ''));
+                            $id0   = $tipo_eventi_attivi[0][0];
+                            $tipo0 = $tipo_eventi_attivi[0][1];
+                            
+                            $label0 = $nota0.' (id=' . $id0 . ', tipo=' . $tipo0 . ')';
+
+                            echo '<option name="evento" value="'. $tipo_eventi_attivi[0][0] .'">' . $label0 . '</option>';
+                        }
                     ?>
                   </select>
-                                  <small id="eventohelp" class="form-text text-muted">Un solo evento attivo (per trasparenza lo mostriamo ma possiamo anche decidere di non farlo).</small>
+                                  <small id="eventohelp" class="form-text text-muted">Un solo evento attivo.</small>
              
             <?php } else {
             	?>
@@ -91,8 +96,17 @@ require('navbar_up.php');
                      <option value=''>Seleziona un evento tra quelli attivi </option>
                     <?php 
                      for ($i=0;$i<$len;$i++){
+                        if ($sospeso[$i] == 0) {
+                            $nota = trim((string)($nota_eventi_attivi[$i][1] ?? ''));
+                            $id   = $tipo_eventi_attivi[$i][0];
+                            $tipo = $tipo_eventi_attivi[$i][1];
+
+                            $label = $nota.' (id=' . $id . ', tipo=' . $tipo . ')';
+
+                            echo '<option name="evento" value="' . $tipo_eventi_attivi[$i][0] . '">' . $label . '</option>';
+                        }
                       
-                        echo '<option name="evento" value="'.$tipo_eventi_attivi[$i][0].'">'. $tipo_eventi_attivi[$i][1].' (id='.$tipo_eventi_attivi[$i][0].')</option>';
+                        //echo '<option name="evento" value="'.$tipo_eventi_attivi[$i][0].'">'. $tipo_eventi_attivi[$i][1].' (id='.$tipo_eventi_attivi[$i][0].')</option>';
                       }
                     ?>
                   </select>
--- pages/nuovo_sopralluogo_mobile.php
+++ pages/nuovo_sopralluogo_mobile.php
@@ -50,7 +50,14 @@ $subtitle = "Nuovo presidio mobile";
                             <select readonly class="form-control" name="evento" required>
                                 <?php 
                                 for ($i = 0; $i < $len; $i++) {
-                                    echo '<option value="' . $tipo_eventi_attivi[0][0] . '">' . $tipo_eventi_attivi[0][1] . ' (id=' . $tipo_eventi_attivi[0][0] . ')</option>';
+                                    $nota0 = trim((string)($nota_eventi_attivi[0][1] ?? ''));
+                                    $id0   = $tipo_eventi_attivi[0][0];
+                                    $tipo0 = $tipo_eventi_attivi[0][1];
+                                    
+                                    $label0 = $nota0.' (id=' . $id0 . ', tipo=' . $tipo0 . ')';
+
+                                    echo '<option name="evento" value="'. $tipo_eventi_attivi[0][0] .'">' . $label0 . '</option>';
+                                    // echo '<option value="' . $tipo_eventi_attivi[0][0] . '">' . $tipo_eventi_attivi[0][1] . ' (id=' . $tipo_eventi_attivi[0][0] . ')</option>';
                                 } 
                                 ?>
                             </select>
@@ -60,7 +67,14 @@ $subtitle = "Nuovo presidio mobile";
                                 <option value="">Seleziona un evento tra quelli attivi</option>
                                 <?php 
                                 for ($i = 0; $i < $len; $i++) {
-                                    echo '<option value="' . $tipo_eventi_attivi[$i][0] . '">' . $tipo_eventi_attivi[$i][1] . ' (id=' . $tipo_eventi_attivi[$i][0] . ')</option>';
+                                    $nota = trim((string)($nota_eventi_attivi[$i][1] ?? ''));
+                                    $id   = $tipo_eventi_attivi[$i][0];
+                                    $tipo = $tipo_eventi_attivi[$i][1];
+
+                                    $label = $nota.' (id=' . $id . ', tipo=' . $tipo . ')';
+
+                                    echo '<option name="evento" value="' . $tipo_eventi_attivi[$i][0] . '">' . $label . '</option>';
+                                    // echo '<option value="' . $tipo_eventi_attivi[$i][0] . '">' . $tipo_eventi_attivi[$i][1] . ' (id=' . $tipo_eventi_attivi[$i][0] . ')</option>';
                                 }
                                 ?>
                             </select>
