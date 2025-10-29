bda805d5b23ce05c5d18f10da8b7b24199291e73
Gter <info@gter.it>
2025-10-29 09:18:30 +0100

    aggiunto nome evento menu a tendina gestione squadre

--- pages/gestione_squadre.php
+++ pages/gestione_squadre.php
@@ -96,8 +96,13 @@ require('navbar_up.php');
 				                 
 				                    <?php 
 				                     for ($i=0;$i<$len;$i++){
-				                      
-				                        echo '<option name="evento" value="'.$tipo_eventi_attivi[0][0].'">'. $tipo_eventi_attivi[0][1].' (id='.$tipo_eventi_attivi[0][0].')</option>';
+                                        $nota0 = trim((string)($nota_eventi_attivi[0][1] ?? ''));
+                                        $id0   = $tipo_eventi_attivi[0][0];
+                                        $tipo0 = $tipo_eventi_attivi[0][1];
+                                        
+                                        $label0 = $nota0.' (id=' . $id0 . ', tipo=' . $tipo0 . ')';
+
+                                        echo '<option name="evento" value="'. $tipo_eventi_attivi[0][0] .'">' . $label0 . '</option>';
 				                      }
 				                    ?>
 				                  </select>
@@ -110,8 +115,13 @@ require('navbar_up.php');
 				                     <option value=''>Seleziona un evento tra quelli attivi </option>
 				                    <?php 
 				                     for ($i=0;$i<$len;$i++){
-				                      
-				                        echo '<option name="evento" value="'.$tipo_eventi_attivi[$i][0].'">'. $tipo_eventi_attivi[$i][1].' (id='.$tipo_eventi_attivi[$i][0].')</option>';
+                                        $nota = trim((string)($nota_eventi_attivi[$i][1] ?? ''));
+                                        $id   = $tipo_eventi_attivi[$i][0];
+                                        $tipo = $tipo_eventi_attivi[$i][1];
+
+                                        $label = $nota.' (id=' . $id . ', tipo=' . $tipo . ')';
+
+                                        echo '<option name="evento" value="' . $tipo_eventi_attivi[$i][0] . '">' . $label . '</option>';
 				                      }
 				                    ?>
 				                  </select>
