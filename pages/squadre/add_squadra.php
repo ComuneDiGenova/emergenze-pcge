ee86b936e04655b195e10d569deeb56fa55d21a1
Gter <info@gter.it>
2026-05-06 17:33:00 +0200

    ora inserisce solo se non esiste già una riga attiva (data_end IS NULL) per la stessa coppia (id_squadra, matricola_cf)

--- pages/squadre/add_squadra.php
+++ pages/squadre/add_squadra.php
@@ -12,19 +12,25 @@ include explode('emergenze-pcge',getcwd())[0].'emergenze-pcge/conn.php';
 $id_squadra=$_GET['s'];
 
 $matricola_cf=$_POST['cf'];
+// Normalizza input (evita doppie submit e SQL injection basilare)
+$id_squadra = (int)$id_squadra;
+$matricola_cf = pg_escape_string($conn, $matricola_cf);
 
-echo "Id squadra=".$id_squadra."<br>";
-echo "matricola_cf=".$matricola_cf."<br>";
-//exit;
-
-$query="INSERT INTO users.t_componenti_squadre(id_squadra, matricola_cf) 
-VALUES (".$id_squadra.", '".$matricola_cf."');";
-echo $query;
-//exit;
+// Inserimento idempotente: evita duplicati "attivi" (data_end IS NULL)
+$query="INSERT INTO users.t_componenti_squadre(id_squadra, matricola_cf)
+SELECT ".$id_squadra.", '".$matricola_cf."'
+WHERE NOT EXISTS (
+	SELECT 1
+	FROM users.t_componenti_squadre
+	WHERE id_squadra = ".$id_squadra."
+	  AND matricola_cf = '".$matricola_cf."'
+	  AND data_end IS NULL
+);";
 $result=pg_query($conn, $query);
-echo "<br>";
 
 
+$mail = '';
+$telefono = '';
 $query_mail="SELECT mail, telefono1 FROM users.v_utenti_esterni WHERE cf='".$matricola_cf."';";
 $result_mail=pg_query($conn, $query_mail);
 while($r_mail= pg_fetch_assoc($result_mail)) { 
@@ -32,24 +38,31 @@ while($r_mail= pg_fetch_assoc($result_mail)) {
 	$telefono=$r_mail['telefono1'];
 }
 
-echo $query_mail."<br>";
-
 
 if ($mail!=''){
-	$query="INSERT INTO users.t_mail_squadre(cod, matricola_cf, mail) 
-	VALUES (".$id_squadra.", '".$matricola_cf."','".$mail."');";
-	echo $query;
-	//exit;
+	$mail_esc = pg_escape_string($conn, $mail);
+	$query="INSERT INTO users.t_mail_squadre(cod, matricola_cf, mail)
+	SELECT ".$id_squadra.", '".$matricola_cf."', '".$mail_esc."'
+	WHERE NOT EXISTS (
+		SELECT 1 FROM users.t_mail_squadre
+		WHERE cod = ".$id_squadra."
+		  AND mail = '".$mail_esc."'
+	);";
 	$result=pg_query($conn, $query);
 }
 
 
 
 if ($telefono!=''){
-	$query="INSERT INTO users.t_telefono_squadre(cod, matricola_cf, telefono) 
-	VALUES (".$id_squadra.", '".$matricola_cf."','".$telefono."');";
-	echo $query;
-	//exit;
+	$telefono_esc = pg_escape_string($conn, $telefono);
+	$query="INSERT INTO users.t_telefono_squadre(cod, matricola_cf, telefono)
+	SELECT ".$id_squadra.", '".$matricola_cf."', '".$telefono_esc."'
+	WHERE NOT EXISTS (
+		SELECT 1 FROM users.t_telefono_squadre
+		WHERE cod = ".$id_squadra."
+		  AND matricola_cf = '".$matricola_cf."'
+		  AND telefono = '".$telefono_esc."'
+	);";
 	$result=pg_query($conn, $query);
 }
 
